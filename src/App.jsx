5f30e3bd5b301e611acce4f812aaceb872aaec98
kkupgrader <kkupgrader.fs@gmail.com>
2022-12-27 14:17:47 +0530

    modal added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import PopUp1 from "./components/PopUp1"
 import PopUp2 from "./components/PopUp2"
 import PopUp3 from "./components/PopUp3"
+import Modal from "./components/Modal"
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <PopUp1 />
             <PopUp2 />
             <PopUp3 />
+            <Modal />
         </>
     )
 }
