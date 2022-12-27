6e403a3643aab2d57f3c4ce64a0aa7d156af9dfc
kkupgrader <kkupgrader.fs@gmail.com>
2022-12-27 14:21:01 +0530

    modal added

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,7 +12,7 @@ export default function Modal() {
             </button>
             <div className={classes.Modal + (open ? " " + classes.isOpen : "")}>
                 <div className={classes.Header}>
-                    <svg stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M405 136.798L375.202 107 256 226.202 136.798 107 107 136.798 226.202 256 107 375.202 136.798 405 256 285.798 375.202 405 405 375.202 285.798 256z" /></svg>
+                    <svg onClick={() => setOpen(false)} stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M405 136.798L375.202 107 256 226.202 136.798 107 107 136.798 226.202 256 107 375.202 136.798 405 256 285.798 375.202 405 405 375.202 285.798 256z" /></svg>
                 </div>
 
                 <div className={classes.Body}>
